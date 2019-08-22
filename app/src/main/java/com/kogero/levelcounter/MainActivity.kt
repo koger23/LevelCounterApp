944316bc1a8be34edbfcbc1c4dc23651d512cf24
Gergely Kocsis <koger23@gmail.com>
2019-08-22 14:16:09 +0200

    fix(*): correct id-s

--- app/src/main/java/com/kogero/levelcounter/MainActivity.kt
+++ app/src/main/java/com/kogero/levelcounter/MainActivity.kt
@@ -2,11 +2,9 @@ package com.kogero.levelcounter
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
 
