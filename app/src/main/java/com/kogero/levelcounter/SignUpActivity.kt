5e317d9852bc1f3513a91ede7a0bb43a0bf6aae8
Gergely Kocsis <koger23@gmail.com>
2019-08-23 02:02:00 +0200

    feat(*): implement signup() in activity

--- app/src/main/java/com/kogero/levelcounter/SignUpActivity.kt
+++ app/src/main/java/com/kogero/levelcounter/SignUpActivity.kt
@@ -2,13 +2,12 @@ package com.kogero.levelcounter
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.kogero.levelcounter.model.requests.LoginRequest
 import com.kogero.levelcounter.model.requests.SignUpRequest
 import com.kogero.levelcounter.model.responses.LoginResponse
+import com.kogero.levelcounter.model.responses.SignUpResponse
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -28,18 +27,60 @@ class SignUpActivity : AppCompatActivity() {
             val email = findViewById<EditText>(R.id.editTextEmail).text.toString()
             val password = findViewById<EditText>(R.id.editTextPassword).text.toString()
             val verifyPassword = findViewById<EditText>(R.id.editTextVerifyPassword).text.toString()
+            val male = findViewById<RadioButton>(R.id.radioBtnMale).isChecked
+            var gender = "female"
+            if (male) {
+                gender = "male"
+            }
 
             if (checkInputs(username, fullName, email, password, verifyPassword)) {
                 Toast.makeText(
                     this@SignUpActivity,
-                    "Signing up...",
+                    "Signing up... $gender",
                     Toast.LENGTH_SHORT
                 ).show()
+
+                val signUpRequest = SignUpRequest(
+                    fullName = fullName,
+                    userName = username,
+                    email = email,
+                    password = password,
+                    verifyPassword = verifyPassword,
+                    gender = gender
+                )
+                signUp(signUpRequest)
             }
         }
     }
 
-    private fun checkInputs(username: String, fullName: String, email: String, password: String, verifyPassword: String): Boolean {
+    private fun signUp(signUpRequest: SignUpRequest) {
+        val call: Call<SignUpResponse> = ApiClient.getClient.signUp(signUpRequest)
+        call.enqueue(object : Callback<SignUpResponse> {
+            override fun onResponse(
+                call: Call<SignUpResponse>,
+                response: Response<SignUpResponse>
+            ) {
+                Toast.makeText(this@SignUpActivity, "Code: " + response!!.code(), Toast.LENGTH_SHORT)
+                    .show()
+            }
+
+            override fun onFailure(call: Call<SignUpResponse>, t: Throwable) {
+                Toast.makeText(
+                    this@SignUpActivity,
+                    "Cannot connect to the server.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        })
+    }
+
+    private fun checkInputs(
+        username: String,
+        fullName: String,
+        email: String,
+        password: String,
+        verifyPassword: String
+    ): Boolean {
         if (fullName.isEmpty()) {
             Toast.makeText(
                 this@SignUpActivity,
--- app/src/main/java/com/kogero/levelcounter/service/ApiInterface.kt
+++ app/src/main/java/com/kogero/levelcounter/service/ApiInterface.kt
@@ -3,6 +3,7 @@ package com.kogero.levelcounter.service
 import com.kogero.levelcounter.model.requests.LoginRequest
 import com.kogero.levelcounter.model.requests.SignUpRequest
 import com.kogero.levelcounter.model.responses.LoginResponse
+import com.kogero.levelcounter.model.responses.SignUpResponse
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.Headers
@@ -12,9 +13,9 @@ interface ApiInterface {
 
     @POST("login")
     @Headers("No-Authentication: true", "User-Agent: LevelCounterApp")
-    fun login(@Body LoginRequest: LoginRequest): Call<LoginResponse>
+    fun login(@Body loginRequest: LoginRequest): Call<LoginResponse>
 
     @POST("signup")
     @Headers("No-Authentication: true", "User-Agent: LevelCounterApp")
-    fun signUp(@Body SignUpRequest: SignUpRequest): Call<String>
+    fun signUp(@Body signUpRequest: SignUpRequest): Call<SignUpResponse>
 }
