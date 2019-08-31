279d0a08fa5509fd95fee9ddffb79c79853709b7
Gergely Kocsis <koger23@gmail.com>
2019-08-31 10:25:02 +0200

    feat(GameActivity): init activity

---
+++ app/src/main/java/com/kogero/levelcounter/GameActivity.kt
@@ -0,0 +1,13 @@
+package com.kogero.levelcounter
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class GameActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game)
+    }
+}
