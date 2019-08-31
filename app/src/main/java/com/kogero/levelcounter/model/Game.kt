97348d3e9ca072d472b777dc9f0af308616dbfd6
Gergely Kocsis <koger23@gmail.com>
2019-08-31 10:22:58 +0200

    feat(Game): implement Parcelable to pass to GameActivity

--- app/src/main/java/com/kogero/levelcounter/model/Game.kt
+++ app/src/main/java/com/kogero/levelcounter/model/Game.kt
@@ -1,5 +1,7 @@
 package com.kogero.levelcounter.model
 
+import android.os.Parcel
+import android.os.Parcelable
 import java.sql.Date
 
 data class Game (
@@ -9,4 +11,35 @@ data class Game (
     val inGameUsers: List<InGameUser>,
     val hostingUserId: String,
     val isRunning: Boolean = false
-)
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readInt(),
+        parcel.readLong(),
+        TODO("dateTime"),
+        TODO("inGameUsers"),
+        parcel.readString(),
+        parcel.readByte() != 0.toByte()
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeInt(id)
+        parcel.writeLong(time)
+        parcel.writeString(hostingUserId)
+        parcel.writeByte(if (isRunning) 1 else 0)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Game> {
+        override fun createFromParcel(parcel: Parcel): Game {
+            return Game(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Game?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
