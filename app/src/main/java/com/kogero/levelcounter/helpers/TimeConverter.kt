99820595370b04008980ba063db75214b568d743
Gergely Kocsis <koger23@gmail.com>
2019-09-13 15:34:42 +0200

    feat(TimeConverter): add time conversion form Date? to long

--- app/src/main/java/com/kogero/levelcounter/helpers/TimeConverter.kt
+++ app/src/main/java/com/kogero/levelcounter/helpers/TimeConverter.kt
@@ -9,7 +9,7 @@ internal object TimeConverter {
     Converting a long number to string and show in h:m:s format
      */
 
-    fun convert(time: Long): String {
+    fun convertTimeFromLong(time: Long): String {
         val hours = time / 3600
         val minutes = time % 3600 / 60
         val seconds = time % 60
@@ -24,5 +24,9 @@ internal object TimeConverter {
             e.printStackTrace()
         }
         return null
+    }
+
+    fun convertDateToLong(date: Date?) : Long {
+        return date!!.time
     }
 }
